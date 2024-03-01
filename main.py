7a722fdc4fb0ed5e6218c781a9d2c96efb447a32
DaveONED <158311365+DaveONED@users.noreply.github.com>
2024-03-01 16:30:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
 import paramiko
-
 import os
-
 from datetime import datetime
 
 
@@ -14,9 +12,7 @@ def transfer_execute_download(host, username, password, local_script_path, remot
         # Connect to the remote server via SSH
 
         ssh = paramiko.SSHClient()
-
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
         ssh.connect(host, username=username, password=password)
 
 
@@ -30,26 +26,17 @@ def transfer_execute_download(host, username, password, local_script_path, remot
         # Create "collection" directory on the remote server
 
         remote_collection_dir = os.path.join(remote_script_path, "collection")
-
         log(log_file, f"Creating directory on remote server: {remote_collection_dir}")
-
-
-
         ssh.exec_command(f'mkdir -p {remote_collection_dir}')
-
         log(log_file, f'Executing command: mkdir -p {remote_collection_dir}')
 
     
 
         # Transfer script files from local machine to remote server
+        
         remote_script_path_file = remote_collection_dir + '/Collection.zip'
-
         log(log_file, f"Transferring script file: {local_script_path} to {remote_script_path_file}")
-
-
-
         sftp.put(local_script_path, remote_script_path_file)
-
         log(log_file, f'Executing command: sftp.put {local_script_path} {remote_script_path_file}')
 
 
@@ -57,11 +44,7 @@ def transfer_execute_download(host, username, password, local_script_path, remot
         # Unzip the transferred script file
 
         log(log_file, f"Unzipping transferred script file: {remote_script_path_file}")
-
-
-
         ssh.exec_command(f'unzip -o {remote_script_path_file} -d {remote_collection_dir}')
-
         log(log_file, f'Executing command: unzip -o {remote_script_path_file} -d {remote_collection_dir}')
 
 
@@ -69,13 +52,8 @@ def transfer_execute_download(host, username, password, local_script_path, remot
         # Set permissions for Collect.sh file
 
         collection_file_path= remote_collection_dir + '/Collection/bin/'
-
         log(log_file, "Setting permissions for Collect.sh file")
-
-
-
         ssh.exec_command(f'chmod 775 {os.path.join(collection_file_path, "Collect.sh")}')
-
         log(log_file, f'Executing command: chmod 775 {os.path.join(collection_file_path, "Collection.sh")}')
 
 
@@ -83,13 +61,23 @@ def transfer_execute_download(host, username, password, local_script_path, remot
         # Execute the script on the remote server
 
         #log(log_file, f"Executing script on remote server: {collection_file_path}")
-
-
-
         #ssh.exec_command(f'bash {collection_file_path[:-4]}')
-
         #log(log_file, f'Executing command: bash {collection_file_path[:-4]}')
 
+
+        # Save the output of the script locally on the server
+        
+        #remote_output_file = os.path.join(remote_output_path, os.path.basename(local_script_path) + ".out")
+        #log(log_file, f"Copying output file from remote server to local machine: {remote_output_file}")
+        #ssh.exec_command(f'cp {remote_script_path[:-4]}.out {remote_output_file}')
+        #log(log_file, f'Executing command: cp {remote_script_path[:-4]}.out {remote_output_file}')
+
+        
+        # Transfer the output file from the remote server to local machine
+        
+        #log(log_file, f"Transferring output file from remote server to local machine: {remote_output_file}")
+        #sftp.get(remote_output_file, local_output_path)
+        #log(log_file, f'Executing command: sftp.get {remote_output_file} {local_output_path}')
 
 
         # Close the SFTP session and SSH connection
