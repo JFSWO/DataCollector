98b13e0a962e379df16ff5928897f6fccef8d554
DaveONED <158311365+DaveONED@users.noreply.github.com>
2024-05-16 12:19:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import paramiko
+
+
+
 import os
 
+
+
+import stat
+
+
+
 from datetime import datetime
+
 
 
 def upload_directory(local_dir, remote_dir, sftp):
 
     for item in os.listdir(local_dir):
+
         local_path = os.path.join(local_dir, item)
+
         remote_path = os.path.join(remote_dir, item)
 
         if os.path.isdir(local_path):
+
             try:
+
                 sftp.mkdir(remote_path)
 
             except OSError as e:
@@ -24,80 +38,250 @@ def upload_directory(local_dir, remote_dir, sftp):
 
             sftp.put(local_path, remote_path)
 
+            
+
+#def download_directory(remote_dir, local_dir, sftp):
+
+    # Ensure local directory exists
+
+ #   os.makedirs(local_dir, exist_ok=True)
+
+
+
+    # List contents of remote directory
+
+  #  dir_items = sftp.listdir_attr(remote_dir)
+
+
+
+   # for item in dir_items:
+
+    #    remote_path = os.path.join(remote_dir, item.filename)
+
+     #   local_path = os.path.join(local_dir, item.filename)
+
+
+
+      #  if stat.S_ISDIR(item.st_mode):
+
+            # Recursively download subdirectory
+
+       #     download_directory(remote_path, local_path, sftp)
+
+        #else:
+
+            # Download file
+
+         #   sftp.get(remote_path, local_path)
+
+         
+
+def extract_file(remote_path, local_path, sftp):
+
+    try:
+
+        # Download the file from the remote server
+
+        sftp.get(remote_path, local_path)
+
+        print(f"File extracted successfully: {local_path}")
+
+    except Exception as e:
+
+        print(f"An error occurred while extracting the file: {e}")
+
 
 
 def transfer_execute_download(host, username, password, local_script_path,local_output_path, remote_script_path, log_file):
 
+
+
     try:
 
+
+
         # Connect to the remote server via SSH
+
+
+
         ssh = paramiko.SSHClient()
+
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
         ssh.connect(host, username=username, password=password)
 
+
+
+
+
+
+
         # Open an SFTP session
+
+
+
         sftp = ssh.open_sftp()
 
 
+
+
+
+
+
         # Create "collection" directory on the remote server
-        remote_collection_dir = os.path.join(remote_script_path, "collection")
+
+
+
+        remote_collection_dir = os.path.join(remote_script_path, "Collection")
+
         log(log_file, f"Creating directory on remote server: {remote_collection_dir}")
+
         ssh.exec_command(f'mkdir -p {remote_collection_dir}')
+
         log(log_file, f'Executing command: mkdir -p {remote_collection_dir}')
 
 
 
+    
+
+
+
         # Transfer script files from local machine to remote server
+
+        
+
         remote_script_path_file = remote_collection_dir + '/Collection.zip'
+
         log(log_file, f"Transferring script file: {local_script_path} to {remote_script_path_file}")
+
         sftp.put(local_script_path, remote_script_path_file)
+
         log(log_file, f'Executing command: sftp.put {local_script_path} {remote_script_path_file}')
 
 
 
 
+
+
+
         # Unzip the transferred script file
+
+
+
         log(log_file, f"Unzipping transferred script file: {remote_script_path_file}")
+
         ssh.exec_command(f'unzip -o {remote_script_path_file} -d {remote_collection_dir}')
+
         log(log_file, f'Executing command: unzip -o {remote_script_path_file} -d {remote_collection_dir}')
 
 
+
+
+
+
+
         # Set permissions for Collect.sh file
+
+
+
         collection_file_path= remote_collection_dir + '/Collection/bin/'
+
         log(log_file, "Setting permissions for Collect.sh file")
+
         ssh.exec_command(f'chmod 775 {os.path.join(collection_file_path, "Collection.sh")}')
+
         log(log_file, f'Executing command: chmod 775 {os.path.join(collection_file_path, "Collect.sh")}')
 
 
 
+
+
+
+
         # Execute the script on the remote server
+
+
+
         log(log_file, f"Executing script on remote server: {collection_file_path}")
+
         ssh.exec_command(f'bash {os.path.join(collection_file_path, "Collection.sh")}')
 
-   
-	    # Transfer the output file from the remote server to local machine
-        remote_output_path= remote_collection_dir + '/Collection/bin/output'
-        upload_directory(local_output_path,remote_output_path,sftp)
-        log(log_file, f"Transferring output file from remote server to local machine: {local_output_path}")
+        
+
+     	
+
+	# Transfer the output file from the remote server to local machine
+
+        remote_output_path_file = remote_collection_dir +'/Collection/bin/output/Collection-pc1_all.tar'
+
+        local_output_path_file = local_output_path + host +'_all.tar'
+
+        extract_file(remote_output_path_file,local_output_path_file,sftp)
 
        
+
+        log(log_file, f"Transferring output file from remote server to local machine: {local_output_path}")
+
+        
+
+        
+
+	# Transfer the output file from the remote server directory to local machine directory
+
+        #remote_output_path = remote_collection_dir +'/Collection/bin/output/'
+
+        #local_output_path = local_output_path +'/'+ host 
+
+        #extract_file(remote_output_path_file,local_output_path_file,sftp)
+
+        #download_directory(remote_output_path_file,local_output_path_file,sftp)
+
+        #log(log_file, f"Transferring output directory from remote server to local machine: {local_output_path}")
+
+       
+
+
+
         # Close the SFTP session and SSH connection
+
+
+
         sftp.close()
+
+
+
         ssh.close()
+
+
+
 
 
 
 
     except Exception as e:
 
+
+
         error_msg = f"An error occurred: {str(e)}"
+
+
+
         log(log_file, error_msg)
+
+
+
         print(error_msg)
 
 
 
         # Close the SSH connection if it's established
+
+
+
         if ssh:
+
+
+
             ssh.close()
 
 
@@ -107,29 +291,113 @@ def transfer_execute_download(host, username, password, local_script_path,local_
 
 
 def log(log_file, message):
+
+
+
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+
     with open(log_file, 'a') as f:
+
+
+
         f.write(f"[{current_time}] {message}\n")
 
 
 
+
+
+
+
 def read_credentials_from_file(file_path):
+
+
+
+
+
+
+
     credentials = []
+
+
+
+
+
+
+
     with open(file_path, 'r') as file:
+
+
+
+
+
+
+
         for line in file:
+
+
+
+
+
+
+
             host, username, password, local_script_path,local_output_path, remote_script_path = line.strip().split(',')  # Adjust delimiter as needed
+
+
+
+
+
+
+
             credentials.append({'host': host, 'username': username, 'password': password,'local_script_path':local_script_path,'local_output_path':local_output_path,'remote_script_path':remote_script_path})
+
+
+
+
+
+
 
     return credentials
 
 
 
+
+
+
+
+
+
+
+
 file_path = 'config.txt'
+
+
+
 log_file = 'transfer_log.txt'
+
+
+
+
+
+
+
 credentials = read_credentials_from_file(file_path)
 
 
+
+
+
+
+
 for cred in credentials:
+
+
+
+
+
+
+
     transfer_execute_download(cred['host'],cred['username'],cred['password'],cred['local_script_path'],cred['local_output_path'],cred['remote_script_path'],log_file)
 
 
